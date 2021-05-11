5783e11aab248bee103990e383c01f74d7eb0d0b
Dacian Tamasan <tamasan.dacian@gmail.com>
2021-05-11 15:47:45 +0200

    Added additional functions to load and save data

--- shared/utils.py
+++ shared/utils.py
@@ -1,6 +1,7 @@
 import contextlib
 import fasttext
 import pickle
+import json
 import re
 import os
 
@@ -13,6 +14,36 @@ def make_dirs(path):
     """
     if not os.path.exists(path):
         os.makedirs(path)
+
+def dump_json(data, filepath, indent=4, sort_keys=True):
+    """ Dump dictionary to json file to a given filepath name 
+    
+    :param data: python dictionary
+    :param filepath: filepath name
+    :param indent: indent keys in json file
+    :param sort_keys: boolean flag to sort keys
+    """
+    with open(filepath, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=indent, sort_keys=sort_keys)
+
+def load_json(filepath):
+    """ Load json file from filepath into a dictionary 
+    
+    :param filepath:
+    :return: python dictionary
+    """
+    data = dict()
+    with open(filepath) as data_file:    
+        data = json.load(data_file)
+    return data
+
+def dump_txt(data, filepath):
+    """ Dump data to txt file format to a given filepath name 
+    
+    :param filepath: filepath name
+    """
+    with open(filepath, "w") as file :
+        file.write(data)
 
 def load_pickle(filepath):
     """ Load data from pickle file from filepath name 
